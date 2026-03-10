10f4b76672d593a46a99ff99aec92313a4cbd7c3
GooseCool1 / Какой-то кот <dp2pn@proton.me>
2026-03-10 21:42:49 +0500

    Га.

---
+++ AfigetDermo/!includes.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <unistd.h>
+
+#include <string_view>
+#include <array>
+#include <cstdint>
+/*
+== WINDOWS MinGW thing ==
+#include <winsock2.h>
+#include <ws2tcpip.h>
+*/
+
+// I like WSL~~... UwU
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "getMIME.hpp"
